f8fc0023cfb60113c1d0b510adb1533be11e0d1c
vaishnavi shelar <97875844+vaishnavishelar110@users.noreply.github.com>
2024-12-21 23:18:54 +0530

    Add files via upload

---
+++ Datastructure/program311.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+ int data;
+ struct node *next;
+
+};
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE head ,int no)
+{
+    PNODE newn=NULL;
+    newn =(PNODE)malloc(sizeof(NODE));
+    newn->data=no;
+    newn->next=NULL;
+    if(*head==NULL)
+    {
+        *head=newn;
+    }
+    else
+    {
+        newn->next=*head;
+        *head=newn;
+    }
+}
+void InsertLast(PPNODE head ,int no)
+{
+    PNODE newn=NULL;
+    PNODE temp=NULL;
+    newn =(PNODE)malloc(sizeof(NODE));
+    newn->data=no;
+    newn->next=NULL;
+    if(*head==NULL)
+    {
+        *head=newn;
+    }
+    else
+    {
+        temp=*head;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=newn;
+    }
+}
+void Display(PNODE head)
+{
+    while(head!=NULL)
+    {
+        printf("| %d | -> ",head->data);
+        head=head->next;
+    }
+    printf("NULL\n");
+}
+int Count(PNODE head)
+{
+    int iCount=0;
+    while(head!=NULL)
+    {
+      iCount++;
+        head=head->next;
+    }
+    return iCount;
+}
+void DeleteFirst(PPNODE head)
+{
+    PNODE temp=NULL;
+    if(*head==NULL)
+    {
+        return;
+    }
+    else if((*head)->next==NULL)
+    {
+        free(*head);
+        *head=NULL;
+    }
+    else{
+        temp=*head;
+        *head=(*head)->next;
+        free(temp);
+    }
+}
+void DeleteLast(PPNODE head)
+{
+    PNODE temp=*head;
+    if(*head==NULL)
+    {
+        return;
+    }
+    else if((*head)->next==NULL)
+    {
+        free(*head);
+        *head=NULL;
+    }
+    else{
+        temp=*head;
+        while(temp->next->next!=NULL)
+        {
+            temp=temp->next;
+
+        }
+         free(temp->next);
+        temp->next=NULL;
+       
+    }
+}
+int main()
+{
+    
+    PNODE first =NULL;
+    InsertFirst(&first,51);
+    InsertFirst(&first,21);
+    InsertFirst(&first,11);
+     InsertLast(&first,101);
+      InsertLast(&first,111);
+        Display(first);
+        int iRet=Count(first);
+        printf("\nnum of elements are :%d\n",iRet);
+        DeleteFirst(&first);
+           DeleteLast(&first);
+         Display(first);
+         iRet=Count(first);
+        printf("\nnum of elements are :%d\n",iRet);
+     
+
+    return 0;
+}
